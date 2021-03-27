aea3fc2830146a4498470fd982659508d61b98e9
Natoandro <anatoandro@hotmail.com>
2021-03-27 10:04:34 +0300

    Update gtest.cmake: create macro add_gtest

--- gtest.cmake
+++ gtest.cmake
@@ -19,8 +19,13 @@ if(result)
   message(FATAL_ERROR "Build step for googletest failed: ${result}")
 endif()
 
-set(gtest_forst_shared_crt ON CACHE BOOL "" FORCE)
-
 add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
                  ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
                  EXCLUDE_FROM_ALL)
+
+macro(add_gtest TESTNAME)
+  add_executable(${TESTNAME} ${ARGN})
+  target_link_libraries(${TESTNAME} gtest gmock gtest_main)
+  set_target_properties(${TESTNAME} PROPERTIES FOLDER tests)
+  add_test(NAME "${TESTNAME}_test" COMMAND ${TESTNAME})
+endmacro()
